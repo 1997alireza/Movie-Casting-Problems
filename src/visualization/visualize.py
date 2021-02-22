868374931f598d857b15c6db16fee3ad27f63244
Hossein Naderi <hnaderi268@gmail.com>
2021-02-22 09:41:06 -0700

    plot degree histogram and adjacency interactive graph

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -2,29 +2,31 @@ import pandas as pd
 import networkx as nx
 import ast
 from itertools import combinations
+import collections
+import matplotlib.pyplot as plt
+from netwulf import visualize
 
 import paths
 
 pd.set_option('display.max_colwidth', None)
-
-# credits dataframe including casts and crew
-credits = pd.read_csv(paths.the_movies_dataset + '/credits.csv')
-# print(credits.head())
-
-# sample cast entry
-k = credits['cast'][0]
-res = ast.literal_eval(k)
-# print(res[0])
-
-# creating graph based on co-acting
-MG = nx.MultiGraph()
 
 
 def rSubset(arr):
     return list(combinations(arr, 2))
 
 
-for i in range(1000):
+# credits dataframe including casts and crew
+credits = pd.read_csv(paths.the_movies_dataset + '/credits.csv')
+# print(credits.head())
+
+# sample cast entry - just warm up!
+k = credits['cast'][0]
+res = ast.literal_eval(k)
+# print(res[0])
+
+# creating graph based on co-acting
+MG = nx.Graph()
+for i in range(10):
     cast = credits['cast'][i]
     res = ast.literal_eval(cast)
     cast_mem = []
@@ -34,7 +36,26 @@ for i in range(1000):
     for k in range(len(edges)):
         MG.add_edge(edges[k][0], edges[k][1])
 
+
+# some basic network properties
 print('no of nodes: ', len(MG.nodes()))
 print('no of edges: ', len(MG.edges()))
 conn_comp = list(nx.connected_components(MG))
 print('no of connected components:', len(conn_comp))
+degree_sequence = sorted([len(n) for n in conn_comp], reverse=True)
+print(degree_sequence[:5])
+print('The highly connected componenet has no of nodes : ', degree_sequence[0])
+
+# degree histogram
+degree_sequence = sorted([d for n, d in MG.degree()], reverse=True)
+degreeCount = collections.Counter(degree_sequence)
+deg, cnt = zip(*degreeCount.items())
+plt.figure(figsize=(20, 10))
+plt.plot(deg, cnt)
+plt.title("Degree Histogram")
+plt.ylabel("Count")
+plt.xlabel("Degree")
+plt.xlim(0, 200)
+
+plt.show()
+visualize(MG)
