712cfe0240ecbd988259479eb0911aa4afcf14de
Hossein Naderi <hnaderi268@gmail.com>
2021-03-01 09:43:34 -0700

    add weighting to actors network graph and make it selective

--- src/modelling/actors_network.py
+++ src/modelling/actors_network.py
@@ -6,8 +6,6 @@ import collections
 import matplotlib.pyplot as plt
 from netwulf import visualize
 
-import paths
-
 pd.set_option('display.max_colwidth', None)
 
 
@@ -26,7 +24,7 @@ res = ast.literal_eval(k)
 
 # creating graph based on co-acting
 MG = nx.Graph()
-for i in range(50):
+for i in range(500):
     cast = credits['cast'][i]
     res = ast.literal_eval(cast)
     cast_mem = []
@@ -34,8 +32,12 @@ for i in range(50):
         cast_mem.append(res[j]['name'])
     edges = rSubset(cast_mem)
     for k in range(len(edges)):
-        MG.add_edge(edges[k][0], edges[k][1])
-
+        if MG.has_edge(edges[k][0], edges[k][1]):
+            MG.add_edge(edges[k][0], edges[k][1], weight=MG[edges[k][0]][edges[k][1]]["weight"] + 1)
+        else:
+            MG.add_edge(edges[k][0], edges[k][1], weight=1)
+        if MG[edges[k][0]][edges[k][1]]["weight"] > 1:
+            print(i)
 
 # some basic network properties
 print('no of nodes: ', len(MG.nodes()))
@@ -47,15 +49,19 @@ print(degree_sequence)
 print('The highly connected componenet has no of nodes : ', degree_sequence[0])
 
 # degree histogram
-degree_sequence = sorted([d for n, d in MG.degree()], reverse=True)
-degreeCount = collections.Counter(degree_sequence)
-deg, cnt = zip(*degreeCount.items())
-plt.figure(figsize=(20, 10))
-plt.plot(deg, cnt)
-plt.title("Degree Histogram")
-plt.ylabel("Count")
-plt.xlabel("Degree")
-plt.xlim(0, 300)
+# degree_sequence = sorted([d for n, d in MG.degree()], reverse=True)
+# degreeCount = collections.Counter(degree_sequence)
+# deg, cnt = zip(*degreeCount.items())
+# plt.figure(figsize=(20, 10))
+# plt.plot(deg, cnt)
+# plt.title("Degree Histogram")
+# plt.ylabel("Count")
+# plt.xlabel("Degree")
+# plt.xlim(0, 300)
 
-plt.show()
-visualize(MG)
+# plt.show()
+
+elarge = [(u, v) for (u, v, d) in MG.edges(data=True) if d['weight'] > 1]
+G=nx.Graph()
+G.add_edges_from(elarge)
+visualize(G)
