519a89b0b430ac701bfa70eb55d7265f6f31d24f
1997alireza <torabian.alireza@gmail.com>
2021-03-25 21:55:18 -0400

    removing unused functions and models in LoNGAE

--- src/modelling/LoNGAE/models/ae.py
+++ src/modelling/LoNGAE/models/ae.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy.sparse as sp
 import tensorflow as tf
 from keras.layers import Input, Dense, Dropout, Lambda, add, Softmax, Activation
 from keras.models import Model
@@ -93,68 +92,6 @@ def create_weighted_cosine_similarity(weights):
     return weighted_cosine_similarity
 
 
-def autoencoder(dataset, adj, weights=None):
-    h, w = adj.shape
-    sparse_net = dataset in ['conflict', 'metabolic', 'protein']
-
-    kwargs = dict(
-        use_bias=True,
-        kernel_initializer='glorot_normal',
-        kernel_regularizer=None,
-        bias_initializer='zeros',
-        bias_regularizer=None,
-        trainable=True,
-    )
-
-    data = Input(shape=(w,), dtype=np.float32, name='data')
-    if sparse_net:
-        # for conflict, metabolic, protein networks
-        noisy_data = Dropout(rate=0.2, name='drop0')(data)
-    else:
-        # for citation, blogcatalog, arxiv-grqc, and powergrid networks
-        noisy_data = Dropout(rate=0.5, name='drop0')(data)
-
-    ### First set of encoding transformation ###
-    encoded = Dense(256, activation='relu',
-            name='encoded1', **kwargs)(noisy_data)
-    if sparse_net:
-        encoded = Lambda(mvn, name='mvn1')(encoded)
-        encoded = Dropout(rate=0.5, name='drop1')(encoded)
-    
-    ### Second set of encoding transformation ###
-    encoded = Dense(128, activation='relu',
-            name='encoded2', **kwargs)(encoded)
-    if sparse_net:
-        encoded = Lambda(mvn, name='mvn2')(encoded)
-    encoded = Dropout(rate=0.5, name='drop2')(encoded)
-
-    # the encoder model maps an input to its encoded representation
-    encoder = Model([data], encoded)
-    encoded1 = encoder.get_layer('encoded1')
-    encoded2 = encoder.get_layer('encoded2')
-    
-    ### First set of decoding transformation ###
-    decoded = DenseTied(256, tie_to=encoded2, transpose=True,
-            activation='relu', name='decoded2')(encoded)
-    if sparse_net:
-        decoded = Lambda(mvn, name='mvn3')(decoded)
-        decoded = Dropout(rate=0.5, name='drop3')(decoded)
-    
-    ### Second set of decoding transformation - reconstruction ###
-    decoded = DenseTied(w, tie_to=encoded1, transpose=True,
-            activation='linear', name='decoded1')(decoded)
-    
-    # compile the autoencoder
-    adam = optimizers.Adam(lr=0.001, decay=0.0)
-    autoencoder = Model(inputs=[data], outputs=[decoded])
-    autoencoder.compile(optimizer=adam, loss=mbce)
-    
-    if weights is not None:
-        autoencoder.load_weights(weights)
-
-    return encoder, autoencoder
-
-
 def autoencoder_with_node_features(adj_row_length, features_length, node_features_weight, weights=None):
     h = adj_row_length
     w = adj_row_length + features_length
@@ -223,71 +160,6 @@ def autoencoder_with_node_features(adj_row_length, features_length, node_feature
               'decoded_feats': create_weighted_cosine_similarity(weights=node_features_weight)},
         loss_weights={'decoded_adj': 2.0, 'decoded_feats': 1.0}
     )
-    if weights is not None:
-        autoencoder.load_weights(weights)
-
-    return encoder, autoencoder
-
-
-def autoencoder_multitask(adj, feats, labels, weights=None):
-    adj = sp.hstack([adj, feats])
-    h, w = adj.shape
-
-    kwargs = dict(
-        use_bias=True,
-        kernel_initializer='glorot_normal',
-        kernel_regularizer=None,
-        bias_initializer='zeros',
-        bias_regularizer=None,
-        trainable=True,
-    )
-
-    data = Input(shape=(w,), dtype=np.float32, name='data')
- 
-    ### First set of encoding transformation ###
-    encoded = Dense(256, activation='relu',
-            name='encoded1', **kwargs)(data)
-
-    ### Second set of encoding transformation ###
-    encoded = Dense(128, activation='relu',
-            name='encoded2', **kwargs)(encoded)
-    # if dataset == 'pubmed':  # TODO: remove
-    #     encoded = Dropout(rate=0.5, name='drop')(encoded)
-    # else:
-    # encoded = Dropout(rate=0.8, name='drop')(encoded)
-
-    encoded = Dropout(rate=0.5, name='drop')(encoded)
-
-    # the encoder model maps an input to its encoded representation
-    encoder = Model([data], encoded)
-    encoded1 = encoder.get_layer('encoded1')
-    encoded2 = encoder.get_layer('encoded2')
-
-    ### First set of decoding transformation ###
-    decoded = DenseTied(256, tie_to=encoded2, transpose=True,
-            activation='relu', name='decoded2')(encoded)
-    
-    ### Node classification ###
-    feat_data = Input(shape=(feats.shape[1],))
-    pred1 = Dense(labels.shape[1], activation='linear')(feat_data)
-    pred2 = Dense(labels.shape[1], activation='linear')(decoded)
-    prediction = add([pred1, pred2], name='prediction')
-
-    ### Second set of decoding transformation - reconstruction ###
-    decoded = DenseTied(w, tie_to=encoded1, transpose=True,
-            activation='linear', name='decoded1')(decoded)
-    
-    # compile the autoencoder
-    adam = optimizers.Adam(lr=0.001, decay=0.0)
-    autoencoder = Model(inputs=[data, feat_data],
-                        outputs=[decoded, prediction])
-    autoencoder.compile(
-            optimizer=adam,
-            loss={'decoded1': mbce,
-                  'prediction': masked_categorical_crossentropy},
-            loss_weights={'decoded1': 1.0, 'prediction': 1.0}
-    )
-
     if weights is not None:
         autoencoder.load_weights(weights)
 
