0d7cb3c2309ecc391a8069f53cbb5048ef4b39aa
Hossein Naderi <hnaderi268@gmail.com>
2021-04-05 17:31:25 -0600

    add some more duos and fix typos

--- src/experiments/co_star_suggestion.py
+++ src/experiments/co_star_suggestion.py
@@ -1,8 +1,11 @@
 from src.processing.GAE_on_actors import get_rating_predictor, target_actor_weight
 from src.utils.TM_dataset import actor_id
 
-"""Ranking The 10 Most Iconic Actor Duos from https://screenrant.com/iconic-actor-duos-ranking/"""
-top_duets = [
+"""Union of lists: Ranking The 10 Most Iconic Actor Duos from: https://screenrant.com/iconic-actor-duos-ranking/
+15 Acting Duos Who Often Film Movies Together, and We’re Happy to See Them Too from:
+https://brightside.me/wonder-films/15-acting-duos-who-often-film-movies-together-and-were-happy-to-see-them-too-634760/"""
+
+top_duos = [
     ['Seth Rogen', 'James Franco'],
     ['Chris Evans', 'Scarlett Johansson'],
     ['Will Ferrell', 'John C. Reilly'],
@@ -13,25 +16,33 @@ top_duets = [
     ['Paul Newman', 'Robert Redford'],
     ['Tom Hanks', 'Meg Ryan'],
     ['Robert De Niro', 'Joe Pesci'],
+    ['Martin Freeman', 'Benedict Cumberbatch'],
+    ['Anne Hathaway', 'Jake Gyllenhaal'],
+    ['Ryan Gosling', 'Emma Stone'],
+    ['Steve Carell', 'Christian Bale'],
+    ['Bradley Cooper', 'Jennifer Lawrence'],
+    ['Johnny Depp', 'Helena Bonham Carter'],
+    ['Al Pacino', 'Robert De Niro'],
+    ['Matthew McConaughey', 'Woody Harrelson']
 ]
 
 
 def test_known_costars():
     rating_predictor, actors_id = get_rating_predictor()
-    for duet in top_duets:
+    for duo in top_duos:
         try:
-            duet_0 = actor_id(duet[0])[0]
-            duet_1 = actor_id(duet[1])[0]
-            edges_weights, __ = rating_predictor(duet_0)
+            duo_0 = actor_id(duo[0])[0]
+            duo_1 = actor_id(duo[1])[0]
+            edges_weights, __ = rating_predictor(duo_0)
             print(
-                str(duet[0]) + "->" + str(duet[1]) + ": " + str(target_actor_weight(duet_1, actors_id, edges_weights)))
-            edges_weights, __ = rating_predictor(duet_1)
+                str(duo[0]) + "->" + str(duo[1]) + ": " + str(target_actor_weight(duo_1, actors_id, edges_weights)))
+            edges_weights, __ = rating_predictor(duo_1)
             print(
-                str(duet[1]) + "->" + str(duet[0]) + ": " + str(target_actor_weight(duet_0, actors_id, edges_weights)))
+                str(duo[1]) + "->" + str(duo[0]) + ": " + str(target_actor_weight(duo_0, actors_id, edges_weights)))
             print()
-            
+
         except Exception as e:
-            print(str(duet) + " " + str(e))
+            print(str(duo) + " " + str(e))
 
 
 if __name__ == '__main__':
